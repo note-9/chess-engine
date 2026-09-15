855a5c7e63bd5db9e46e32fe899da3cd9ce055bd
note-9 <heemanshbhawsar2004@gmail.com>
2026-09-15 11:01:25 +0530

    implemented some helper functions

--- chess.c
+++ chess.c
@@ -11,16 +11,22 @@ enum {
   a2, b2, c2, d2, e2, f2, g2, h2,
   a1, b1, c1, d1, e1, f1, g1, h1
 };
-/*
-"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"
-"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"
-"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"
-"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"
-"a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"
-"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"
-"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"
-"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
-*/
+
+enum {
+  white,
+  black
+};
+
+const char *coordinates[] = {
+  "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+  "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+  "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+  "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+  "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+  "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+  "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+  "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+};
 
 static inline void set_bit(uint64_t *bitboard, int square) {
     *bitboard |= (1ULL << square);
@@ -32,6 +38,29 @@ static inline int get_bit(uint64_t bitboard, int square) {
 
 static inline void pop_bit(uint64_t *bitboard, int square) {
     *bitboard &= ~(1ULL << square);
+}
+
+static inline int count_bits(uint64_t bitboard)
+{
+  int count = 0;
+  while(bitboard)
+  {
+    count++;
+    bitboard &= bitboard - 1;
+  }
+  return count;
+}
+
+static inline int get_ls1b_index(uint64_t bitboard)
+{
+  if (bitboard)
+  {
+    return count_bits((bitboard & -bitboard) - 1);
+  }
+  else
+  {
+    return -1;
+  }
 }
 
 void print_bitboard(uint64_t bitboard)
@@ -53,11 +82,193 @@ void print_bitboard(uint64_t bitboard)
   printf("\n    a b c d e f g h\n\n");
   printf("Bitboard: %" PRIu64 "\n", bitboard);
 }
+
+const uint64_t not_file_a = 18374403900871474942ULL;
+const uint64_t not_file_h = 9187201950435737471ULL;
+const uint64_t not_file_hg = 4557430888798830399ULL;
+const uint64_t not_file_ab = 18229723555195321596ULL;
+
+uint64_t pawn_attacks[2][64];
+uint64_t knight_attacks[64];
+uint64_t king_attacks[64];
+uint64_t bishop_attacks[64];
+uint64_t rook_attacks[64];
+
+uint64_t mask_pawn_attacks(int side, int square)
+{
+  uint64_t attacks_bitboard = 0;
+  uint64_t piece_bitboard = 0;
+  set_bit(&piece_bitboard, square);
+  if (!side)
+  {
+    if ((piece_bitboard >> 7) & not_file_a) attacks_bitboard |= (piece_bitboard >> 7);
+    if ((piece_bitboard >> 9) & not_file_h) attacks_bitboard |= (piece_bitboard >> 9);
+  }
+  else
+  {
+    if ((piece_bitboard << 7) & not_file_h) attacks_bitboard |= (piece_bitboard << 7);
+    if ((piece_bitboard << 9) & not_file_a) attacks_bitboard |= (piece_bitboard << 9);
+   
+  } 
+  return attacks_bitboard;
+}
+
+uint64_t mask_knight_attacks(int square)
+{
+  uint64_t attacks_bitboard = 0;
+  uint64_t piece_bitboard = 0;
+  set_bit(&piece_bitboard, square);
+
+  if ((piece_bitboard >> 17) & not_file_h) attacks_bitboard |= (piece_bitboard >> 17);
+  if ((piece_bitboard >> 15) & not_file_a) attacks_bitboard |= (piece_bitboard >> 15);
+  if ((piece_bitboard >> 10) & not_file_hg) attacks_bitboard |= (piece_bitboard >> 10);
+  if ((piece_bitboard >> 6) & not_file_ab) attacks_bitboard |= (piece_bitboard >> 6);
+  if ((piece_bitboard << 17) & not_file_a) attacks_bitboard |= (piece_bitboard << 17);
+  if ((piece_bitboard << 15) & not_file_h) attacks_bitboard |= (piece_bitboard << 15);
+  if ((piece_bitboard << 10) & not_file_ab) attacks_bitboard |= (piece_bitboard << 10);
+  if ((piece_bitboard << 6) & not_file_hg) attacks_bitboard |= (piece_bitboard << 6);
+ 
+    
+  return attacks_bitboard;
+}
+
+
+uint64_t mask_king_attacks(int square)
+{
+  uint64_t attacks_bitboard = 0;
+  uint64_t piece_bitboard = 0;
+  set_bit(&piece_bitboard, square);
+
+  if ((piece_bitboard >> 7) & not_file_a) attacks_bitboard |= (piece_bitboard >> 7);
+  if (piece_bitboard >> 8) attacks_bitboard |= (piece_bitboard >> 8);
+  if ((piece_bitboard >> 9) & not_file_h) attacks_bitboard |= (piece_bitboard >> 9);
+  if ((piece_bitboard >> 1) & not_file_h) attacks_bitboard |= (piece_bitboard >> 1);
+  if ((piece_bitboard << 1) & not_file_a) attacks_bitboard |= (piece_bitboard << 1);
+  if ((piece_bitboard << 9) & not_file_a) attacks_bitboard |= (piece_bitboard << 9);
+  if (piece_bitboard << 8) attacks_bitboard |= (piece_bitboard << 8);
+  if ((piece_bitboard << 7) & not_file_h) attacks_bitboard |= (piece_bitboard << 7);
+ 
+    
+  return attacks_bitboard;
+}
+
+uint64_t mask_bishop_attacks(int square, uint64_t occupied)
+{
+  uint64_t attacks_bitboard = 0;
+
+  int rank, file;
+  int target_rank = square / 8;
+  int target_file = square % 8;
+
+  for (rank = target_rank + 1, file = target_file + 1; rank <= 7 && file <= 7; rank++, file++)
+  {
+    attacks_bitboard |= (1ULL << (rank * 8 + file));
+    if ((1ULL << (rank * 8 + file)) & occupied) break;
+  }
+  for (rank = target_rank - 1, file = target_file + 1; rank >= 0 && file <= 7; rank--, file++)
+  {
+    attacks_bitboard |= (1ULL << (rank * 8 + file));
+    if ((1ULL << (rank * 8 + file)) & occupied) break;
+  }
+  for (rank = target_rank + 1, file = target_file - 1; rank <= 7 && file >= 0; rank++, file--)
+  {
+    attacks_bitboard |= (1ULL << (rank * 8 + file)); 
+    if ((1ULL << (rank * 8 + file)) & occupied) break;
+  }
+  for (rank = target_rank - 1, file = target_file - 1; rank >= 0 && file >= 0; rank--, file--)
+  {
+    attacks_bitboard |= (1ULL << (rank * 8 + file));
+    if ((1ULL << (rank * 8 + file)) & occupied) break;
+  }      
+  return attacks_bitboard;
+}
+
+uint64_t mask_rook_attacks(int square, uint64_t occupied)
+{
+  uint64_t attacks_bitboard = 0;
+
+  int rank, file;
+  int target_rank = square / 8;
+  int target_file = square % 8;
+
+  for (rank = target_rank + 1; rank <= 7; rank++)
+  {
+    attacks_bitboard |= (1ULL << (rank * 8 + target_file));
+    if ((1ULL << (rank * 8 + target_file)) & occupied) break;
+  }
+      
+  for (rank = target_rank - 1; rank >= 0; rank--) 
+  {
+    attacks_bitboard |= (1ULL << (rank * 8 + target_file));
+    if ((1ULL << (rank * 8 + target_file)) & occupied) break;
+  }
+      
+  for (file = target_file + 1; file <= 7; file++)
+  {
+    attacks_bitboard |= (1ULL << (rank * 8 + target_file));
+    if ((1ULL << (rank * 8 + target_file)) & occupied) break;
+  }
+  
+  for (file = target_file - 1; file >= 0; file--)
+  {
+    attacks_bitboard |= (1ULL << (target_rank * 8 + file));
+    if ((1ULL << (rank * 8 + target_file)) & occupied) break;
+  }
+      
+  return attacks_bitboard;
+}
+
+void init_leaper_attacks()
+{
+  for (int square = 0; square < 64; square++)
+  {
+    pawn_attacks[white][square] = mask_pawn_attacks(white, square);
+    pawn_attacks[black][square] = mask_pawn_attacks(black, square);
+
+    knight_attacks[square] = mask_knight_attacks(square);
+    king_attacks[square] = mask_king_attacks(square);
+  }
+}
+
+void init_slider_attacks(uint64_t occupied)
+{
+  for (int square = 0; square < 64; square++)
+  {
+    bishop_attacks[square] = mask_bishop_attacks(square, occupied);
+    rook_attacks[square] = mask_rook_attacks(square, occupied);
+  }
+}
+
 int main()
 {
-  uint64_t bitboard = 0ULL;
-  set_bit(&bitboard, e2);
+  init_leaper_attacks();
+  uint64_t occupied = 0ULL;;
   printf("CHESS ENGINE\n");
-  print_bitboard(bitboard);
+ /* for (int square = 0; square < 64; square++)
+  {
+    print_bitboard(pawn_attacks[black][square]);
+  }
+  */
+/*  for (int square = 0; square < 64; square++)
+  {
+    print_bitboard(knight_attacks[square]);
+  }
+*/
+ 
+/*  for (int square = 0; square < 64; square++)
+  {
+    print_bitboard(king_attacks[square]);
+  }
+*/
+
+/*  for (int square = 0; square < 64; square++)
+  {
+    print_bitboard(bishop_attacks[square]);
+  }
+*/
+  for (int square = 0; square < 64; square++)
+  {
+    print_bitboard(mask_rook_attacks(square, occupied));
+  } 
   return 0;
 }
